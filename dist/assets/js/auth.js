d6c9a76f793004c95717472c1b5dde8e4ef9b027
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-01-11 18:53:43 -0500

    adding header image to homepage

---
+++ dist/assets/js/auth.js
@@ -0,0 +1,10 @@
+/*! auth.js | Bulkit | CSS Ninja */
+
+/* ==========================================================================
+Authentication and registration pages JS file
+========================================================================== */
+$(document).ready(function ($) {
+  "use strict";
+
+  initAuth();
+});
